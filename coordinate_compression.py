534a1ab19d8b8bf93d06f3745643bb512ccae7da
ishikawa16 <shin.0116@keio.jp>
2020-08-28 02:49:37 +0900

    add coordinate_compression

---
+++ coordinate_compression.py
@@ -0,0 +1,23 @@
+# Coordinate Compression
+
+from bisect import bisect_left
+
+
+def compress(a):
+    '''
+    配列の大小関係を抽出する(1次元圧縮)
+    '''
+    tmp = sorted(list(set(a)))
+    res = []
+    for v in a:
+        res.append(bisect_left(tmp, v))
+    
+    return res
+
+
+
+'''
+a = [1, 4, 3, 4, 8, 6]
+compress(a)
+> [0, 2, 1, 2, 4, 3]
+'''
