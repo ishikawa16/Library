ef50a4099224cb114906612fea4f719fd46d6c07
Shintaro Ishikawa <shin@ShintaronoMacBook-Pro.local>
2020-09-09 00:33:13 +0900

    add z_algorithm

---
+++ string/z_algorithm.py
@@ -0,0 +1,40 @@
+def z_algo(s):
+    """Z-algorithm O(|S|)
+
+    Args:
+        s (str): 対象文字列
+    
+    Returns:
+        list: lcp配列 (z[i]: sとs[i:]の最長共通接尾辞の長さ)
+    """
+    n = len(s)
+    z = [0] * n
+    z[0] = n
+    i, j = 1, 0
+    while i < n:
+        while i + j < n and s[j] == s[i+j]:
+            j += 1
+        z[i] = j
+
+        if j == 0:
+            i += 1
+            continue
+
+        k = 1
+        while i + k < n and k + z[k] < j:
+            z[i+k] = z[k]
+            k += 1
+
+        i += k
+        j -= k
+
+    return z
+
+
+
+'''
+<使用例>
+>>> s = 'abracadabra'
+>>> z_algo(s)
+[11, 0, 0, 1, 0, 1, 0, 4, 0, 0, 1]
+'''
